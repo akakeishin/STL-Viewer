2319bfdbbf967b1458e8333d04b98eceebb0395f
akakeishin <mizururi_22@gmail.com>
2018-09-04 00:59:19 +0900

    define stl constructors and functions

--- stl_model.cpp
+++ stl_model.cpp
@@ -1,1 +1,14 @@
 #include "stl_model.h"
+
+STLModel readStlAscii(std::string filePath) {
+
+}
+
+STLModel readStlBinary(std::string filePath) {
+
+}
+
+STLModel readStlModel(std::string filePath) {
+
+    return readStlAscii(filePath);
+}
--- stl_model.h
+++ stl_model.h
@@ -7,24 +7,52 @@
 // 3D vector
 class Vec3d {
 
+private:
     double x;
     double y;
     double z;
+
+public:
+    Vec3d() {
+
+    }
+
+    Vec3d(double x0, double y0, double z0) {
+        x = x0;
+        y = y0;
+        z = z0;
+    }
 };
 
 // Triangle in 3D space
 class Triangle {
 
+private:
     Vec3d normal;
     Vec3d points[3];
+
+public:
+    Triangle(Vec3d n, Vec3d p[3]) {
+        normal = n;
+        *points = *p;
+    }
 };
 
 // STL Model
 class STLModel {
 
+private:
     std::string name;
     std::vector<Triangle> facets;
+
+public:
+    STLModel(std::string n, std::vector<Triangle> f) {
+        name = n;
+        facets = f;
+    }
 };
 
+//STL reader from file
+STLModel readStlModel(std::string filePath);
 
 #endif //STLVIEWER_STL_MODEL_H
